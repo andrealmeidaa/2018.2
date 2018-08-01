d7a4a7dc80c6089eef1ecbf4db16f1803a928ddb
Andre Gustavo Duarte de Almeida <1577655@ifrn.local>
2018-08-01 19:26:21 -0300

    Primeiros códigos Kotlin

---
+++ kotlin/ex02.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>){
+
+    var x:Int=22;
+    println(x)
+    var y:Double=x.toDouble()
+    println(y)
+    var z:String=y.toString()
+    println(z)
+
+}
---
+++ kotlin/ex03.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>){
+    var idade:Int=22
+    var msg:String="A minha idade é $idade"
+    println(msg)
+    msg="O dobra da minha idade é ${idade*2}"
+    println(msg)
+}
